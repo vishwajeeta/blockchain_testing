5f60c5b584559bc09b5fc19b98d2383da8a7b7f1
vishwajeeta <gs699542@gmail.com>
2025-02-25 20:57:38 +0530

    adding number or argument to slot

--- Huff, Yul,contract Oppcode disassembly/section1 horse store/src/yul horse store/yulHorseStore.sol
+++ Huff, Yul,contract Oppcode disassembly/section1 horse store/src/yul horse store/yulHorseStore.sol
@@ -5,6 +5,9 @@ contract HorseStore {
     uint256 numberOfHouses;
     function updateHorseName(uint256 newNumberOfHorses) external {
         // numberOfHouses=newNumberOfHorses;
+        assembly{
+            sstore(numberOfHouses.slot,newNumberOfHorses)
+        }
     }
     function readNumberOfHorses() external view returns(uint256){
         // return numberOfHouses;
