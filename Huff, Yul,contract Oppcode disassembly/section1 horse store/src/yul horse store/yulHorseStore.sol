861aa8633242b74e4c94869208cce985e07fd90b
vishwajeeta <gs699542@gmail.com>
2025-02-25 20:54:32 +0530

    starting with yul

--- Huff, Yul,contract Oppcode disassembly/section1 horse store/src/yul horse store/yulHorseStore.sol
+++ Huff, Yul,contract Oppcode disassembly/section1 horse store/src/yul horse store/yulHorseStore.sol
@@ -4,9 +4,9 @@ pragma solidity 0.8.20;
 contract HorseStore {
     uint256 numberOfHouses;
     function updateHorseName(uint256 newNumberOfHorses) external {
-        numberOfHouses=newNumberOfHorses;
+        // numberOfHouses=newNumberOfHorses;
     }
     function readNumberOfHorses() external view returns(uint256){
-        return numberOfHouses;
+        // return numberOfHouses;
     }
 }
