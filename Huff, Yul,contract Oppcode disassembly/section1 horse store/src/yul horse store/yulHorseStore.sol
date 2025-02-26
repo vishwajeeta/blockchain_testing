67a535e6b58e1d1058da54231160822e60add840
vishwajeeta <gs699542@gmail.com>
2025-02-26 21:53:04 +0530

    get return value in assembly yul

--- Huff, Yul,contract Oppcode disassembly/section1 horse store/src/yul horse store/yulHorseStore.sol
+++ Huff, Yul,contract Oppcode disassembly/section1 horse store/src/yul horse store/yulHorseStore.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-contract HorseStore {
+contract HorseStoreYul {
     uint256 numberOfHouses;
     function updateHorseName(uint256 newNumberOfHorses) external {
         // numberOfHouses=newNumberOfHorses;
